afbf4f71a63b08c751e90d3effb3831c435839fc
Skruppy <skrupellos.spam@web.de>
2014-06-27 03:13:54 +0200

    Added files ignored by master

---
+++ data.json.js
@@ -0,0 +1,202 @@
+var nodeData = {
+	"werbung_mathe_lsfAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_info_lsfZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_physik_webseiteAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_mathe_webseitZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_welcheGer\u00e4te": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Raumbuchungen_zuteilung": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_welchesB\u00fcromaterial": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_gafWebseite": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_aufbauen_reinigungGumbelAufh\u00e4ngen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Schilder_aufh\u00e4ngPlan": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Ablaufplan_modulConstraints": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_wannWoMastertreff": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_facebookPosten": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Raumbuchungen_buchen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_flowControl": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Schilder_abh\u00e4ngPlan": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_mathe_webseiteAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_telefon_beantragen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_facebookErstellen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_telefon_testen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Ablaufplan_modulZeiten": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Schilder_wo": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"weiteres_wegweiserHolen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_physik_lsfZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_info_webseiteAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Ablaufplan_moduleOrdnenVerteilen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_physik_webseitZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Schilder_erstellen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Raumbuchungen_studisProVeranstaltungSch\u00e4tzen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_verkaufsschalter_wann": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_verkaufsschalter_wer": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Ablaufplan_gesamteOPhaseTerminieren": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_mathe_lsfZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Ablaufplan_welcheModule": {
+		"name": null,
+		"description": null,
+		"status": "done"
+	},
+	"weiteres_wegweiserBesprechen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_physik_lsfAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_info_lsfAnpassenLassen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"werbung_info_webseitZust\u00e4ndigerHerausfinden": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_planen_verkaufsschalter_was": {
+		"name": null,
+		"description": null,
+		"status": ""
+	},
+	"Zentrale_aufbauen_studisGumbelAufh\u00e4ngen": {
+		"name": null,
+		"description": null,
+		"status": ""
+	}
+};
